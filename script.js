0fc1839843af88de6a8b081315c637c249b3696d
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-04-17 23:35:41 +0530

    7 commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var name = prompt('what is your name ?');
+
+
+var username = prompt( 'what is your age buddy  ?' );
+var number = Number(username);
+console.log(number + 2);
+
+var colour = prompt ('what is your favourite colour');
