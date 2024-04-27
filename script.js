1086a2321d5241fc21eee8367be7bd26cb20ea85
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-04-27 18:07:03 +0530

    jb practice

--- script.js
+++ script.js
@@ -2,3 +2,114 @@
 // var age = prompt("what is your age buddy?")
 // var age = number ('what is your name buddy?')
 // console.log = (age+1)
+
+// let a = 5
+
+// {
+// let a = 'adioti';
+// console.log(a)
+// }
+
+// console.log(a)
+
+// const author= 'aditi'
+// const harry
+
+// let a = 'nulll' ;
+// let b = '20'
+// let c= true;
+// let d =Symbol(' ia m robot');
+// let e= BigInt(25) + BigInt(5)
+// let f="adiri"
+// let g =  undefined
+// console.log(a, b, c, d , e, f, g)
+// console.log(typeof f)
+
+// const item = {
+// "harry": true,
+// "rohan": false,
+// "aditu" : 23,
+// "aditya": undefined,
+// }
+// console.log(item["aditya"])
+
+// let a = "aditi";
+// let b = 4
+// console.log(a + b)
+
+// console.log (typeof (a+ b))
+
+// const a ={
+// name : "aditi",
+// class: 2,
+
+// }
+// a ["friend"]= "ybd"
+// a ["name"]= "xuh"
+// console.log(a)
+
+
+// const dia = {
+//     Accentuate:"mphasize",
+//     Acquiesce:"Consent",
+//     Align:"Position",
+
+// }
+// console.log(dia.Align)
+
+
+// let a = prompt("what is your age ?")
+// if (a>10 &&  a<20){
+// console.log("your age is lies between 10 and 20")
+// }
+// else{
+//     console.log("your age does not lies between 10 and 20")
+// }
+
+
+
+/////////////////////////////SWITCH//////////////////////////////////
+// let a = prompt("what is your age ?")
+// switch (a) {
+// case "1":
+// console.log("youe is age 1")
+// case"2":
+// console.log("youe is age 2")
+// case   '3':
+// console.log("youe is age 3")
+// break
+
+// default:
+// console.log('your age is not special')
+// }
+
+
+// let num = prompt( "what is your age ?" )
+// num  =  Number.parseInt(num)
+
+// if (  num  %  2==0   &&   num  %  4 == 0)  {
+// console.log( "your age is divisible by  2 and 4" )
+// }
+
+// else{
+//     console.log( "your age is not divisible by 2 and 4" )
+// }
+ 
+// let age = prompt(  "what is your age  ")
+// let a = age>18  ? "you can drive"   :  "you can't drivee "
+// console.log (  a )
+
+// let cn = 7;
+// let i 
+// while (i != cn){
+
+// i  = prompt ("enter a number ")
+// }
+// console.log("you entered correct no ")
+
+
+const mean = (a, b, c, d)=> {
+    return (a + b + c+  d)/4
+}
+
+console.log(mean(4, 5, 6, 7))
